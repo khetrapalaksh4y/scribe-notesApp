88c77f7654cca91520e22c44653476eea2f47440
Akshay <akshay@infeedo.com>
2017-05-14 22:39:06 +0530

    Initialize app

---
+++ src/app/authentication/authentication.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule
+  ],
+  declarations: [LoginComponent, SignUpComponent]
+})
+export class AuthenticationModule { }
